49156c999c0eec4fcace95f7427cf6f2cad29110
daniil432 <lfybbk1399123@gmail.com>
2022-04-27 18:17:44 +0300

    Добавлен js скрипт для вывода графика в координатах главных компонент. Далее будут добавлены остальные типы графиков, а также скрипты будут вынесены из html в отдельные файлы.

--- Graph_App/templates/Graph_App/ScatterGraph.js
+++ Graph_App/templates/Graph_App/ScatterGraph.js
@@ -1,72 +1,31 @@
-var chart;
+var xArray = x_y_data;
+var yArray = x_y_data;
 
-JSC.fetch(
-  './resources/fertilityRateAndLifeExpectancy.csv'
-)
-  .then(function(response) {
-    return response.text();
-  })
-  .then(function(text) {
-    var data = JSC.csv2Json(text);
-    chart = renderChart(data);
-  });
-function renderChart(data) {
-  var series = [
-    {
-      name: '""""""""""""""Category name 1""""""""""""""',
-      type: 'marker',
-      points: data.map(function(item) {
-        return {
-          x: item.fertility_rate1960,
-          y: item.life_expectancy1960,
-          name: item.country
-        };
-      })
-    },
-    {
-      name: '""""""""""""""Category name 2""""""""""""""',
-      type: 'marker',
-      points: data.map(function(item) {
-        return {
-          x: item.fertility_rate1990,
-          y: item.life_expectancy1990,
-          name: item.country
-        };
-      })
-    },
-    {
-      name: '""""""""""""""Category name 3""""""""""""""',
-      type: 'marker',
-      points: data.map(function(item) {
-        return {
-          x: item.___dataforxaxes___,
-          y: item.___dataforxaxes___,
-          name: item.___name___
-        };
-      })
-    }
-  ];
-  return JSC.chart('chartDiv', {
-    axisToZoom: 'xy',
-    defaultPoint: {
-      tooltip:
-        '<b>%"""name of category"""</b><br>%"""x value label""": <b>%xValue</b><br>%"""y value label"""": <b>%yValue</b>',
-      opacity: 0.8,
-      marker: {
-        type: 'circle',
-        outline_width: 0,
-        size: 12
-      }
-    },
-    legend_template: '%icon %name',
-    yAxis: {
-      label_text: '"""""""""""label text y"""""""""""',
-      alternateGridFill: 'none'
-    },
-    xAxis: {
-      scale_range: [0, 10],
-      label_text: '""""""""""label text x""""""""""'
-    },
-    series: series
-  });
-}
+// Define Data
+var data1 = {
+    x: xArray['donor'][0],
+    y: yArray['donor'][1],
+    mode: "markers",
+    type: 'scatter',
+    name: '1'
+};
+
+var data2 = {
+    x: xArray['myeloma'][0],
+    y: yArray['myeloma'][1],
+    mode: "markers",
+    type: 'scatter',
+    name: '2'
+};
+
+var data = [data1, data2];
+
+// Define Layout
+var layout = {
+    xaxis: {title: "PC1"},
+    yaxis: {title: "PC2"},
+    title: "Graph in PC1-PC2 coordinates"
+};
+
+// Display using Plotly
+Plotly.newPlot("myPlot", data, layout);
--- Graph_App/views.py
+++ Graph_App/views.py
@@ -6,6 +6,7 @@ from PCA_app.MathApp.sort_data import SortAndCalcRatio
 from PCA_app.MathApp.pca_analyzation import PcaAnalyzation
 from PCA_app.MathApp.derivative import Derivative
 from Graph_App.forms import ButtonTemplate
+from json import dumps
 import re
 
 
@@ -25,18 +26,6 @@ def Graph(request):
     request.session['second_deriv'] = second_deriv
     request.session['calculated_average'] = calculated_average
     request.session['calculated_samples'] = calculated_samples
-    print('1---------------------------------')
-    print(request.session['t_matrix'])
-    print('2---------------------------------')
-    print(request.session['p_matrix'])
-    print('3---------------------------------')
-    print(request.session['first_deriv'])
-    print('4---------------------------------')
-    print(request.session['second_deriv'])
-    print('5---------------------------------')
-    print(request.session['calculated_average'])
-    print('6---------------------------------')
-    print(request.session['calculated_samples'])
     return render(request, 'Graph_App/GraphWindow.html', context)
 
 
@@ -46,28 +35,23 @@ def testview(request, param):
         condition = 1
         return test(request, header, condition, param)
     elif param == 2:
-        header = "Введите номера главных компонент для 3D:"
-        condition = 1
-        print(param, type(param))
-        return test(request, header, condition, param)
-    elif param == 3:
         header = "Введите номер главной компоненты для графика нагрузок:"
         condition = 1
-        print(param, type(param))
+        return test(request, header, condition, param)
+    elif param == 3:
+        header = "Введите номера главных компонент для 3D:"
+        condition = 1
         return test(request, header, condition, param)
     elif param == 4:
-        header = ''
+        header = 'График средних отношений поглощений:'
         condition = 0
-        print(param, type(param))
         return test(request, header, condition, param)
     elif param == 5:
         header = "Введите номера пациентов для сравнения с донорами:"
         condition = 1
-        print(param, type(param))
         return test(request, header, condition, param)
     else:
         context = {'buttons': ButtonTemplate, 'condition': 1}
-        print(param, type(param))
         return render(request, 'Graph_App/GraphWindow.html', context)
 
 
@@ -84,10 +68,12 @@ def test(request, header, condition, param):
                 else:
                     for ind in range(len(selected_numbers)):
                         selected_numbers[ind] = int(selected_numbers[ind])
-                    print('ya tut')
                     x_y_data = ChooseTypeOfGraph(request, param, selected_numbers)
-                    print('ya uje tut')
                     print(x_y_data, type(x_y_data))
+                    x_y_dataJSON = dumps(x_y_data)
+                    context = {'form': form, 'buttons': ButtonTemplate, 'data': selected_numbers,
+                               'condition': condition, 'header': header, 'x_y_data': x_y_dataJSON}
+                    return render(request, 'Graph_App/GraphWindow.html', context)
             except:
                 selected_numbers = 'Ошибка'
             context = {'form': form, 'buttons': ButtonTemplate, 'data': selected_numbers, 'condition': condition, 'header': header}
@@ -96,6 +82,15 @@ def test(request, header, condition, param):
             selected_numbers = 'Вы ничего не ввели или ввели с данные ошибкой'
             context = {'form': form, 'buttons': ButtonTemplate, 'data': selected_numbers, 'condition': condition, 'header': header}
             return render(request, 'Graph_App/GraphWindow.html', context)
+    elif param == 4:
+        form = NumberForm()
+        selected_numbers = None
+        x_y_data = ChooseTypeOfGraph(request, param, selected_numbers)
+        print(x_y_data, type(x_y_data))
+        x_y_dataJSON = dumps(x_y_data)
+        context = {'form': form, 'buttons': ButtonTemplate, 'data': selected_numbers,
+                   'condition': condition, 'header': header, 'x_y_data': x_y_dataJSON}
+        return render(request, 'Graph_App/GraphWindow.html', context)
     else:
         form = NumberForm()
         context = {'form': form, 'buttons': ButtonTemplate, 'condition': condition, 'header': header}
@@ -150,21 +145,25 @@ def ChooseTypeOfGraph(request, param, selected_numbers):
     unknown = [[], []]
     x_y_data = {}
     filenames = request.session['filenames']
-    if param == (1 or 2 or 3):
-        if param == (1 or 3):
-            selected_matrix = request.session['t_matrix']
-        elif param == 2:
-            selected_matrix = request.session['p_matrix']
+    print(param)
+    if (param == 1) or (param == 3):
+        selected_matrix = request.session['t_matrix']
+        if param == 3:
+            donor_col.append([])
+            mm_col.append([])
+            non_mm_col.append([])
+            unknown.append([])
+        print(type(selected_matrix), len(selected_matrix), len(selected_matrix[0]), len(filenames))
         for ind in range(len(selected_matrix)):
-            if filenames[ind][0] == ('D' or 'H'):
-                donor_col[0].append(selected_matrix[ind][selected_numbers[0]-1])
+            if (filenames[ind][0] == 'D') or (filenames[ind][0] == 'H'):
+                donor_col[0].append(selected_matrix[ind][selected_numbers[0] - 1])
                 donor_col[1].append(selected_matrix[ind][selected_numbers[1] - 1])
                 if param == 3:
                     donor_col[2].append(selected_matrix[ind][selected_numbers[2] - 1])
                 else:
                     pass
-            elif filenames[ind][0] == ('M' or 'P'):
-                mm_col[0].append(selected_matrix[ind][selected_numbers[0]-1])
+            elif (filenames[ind][0] == 'M') or (filenames[ind][0] == 'P'):
+                mm_col[0].append(selected_matrix[ind][selected_numbers[0] - 1])
                 mm_col[1].append(selected_matrix[ind][selected_numbers[1] - 1])
                 if param == 3:
                     mm_col[2].append(selected_matrix[ind][selected_numbers[2] - 1])
@@ -185,30 +184,36 @@ def ChooseTypeOfGraph(request, param, selected_numbers):
                 else:
                     pass
             x_y_data = {"donor": donor_col, "myeloma": mm_col, "non_myeloma": non_mm_col, "unknown": unknown}
+    elif param == 2:
+        selected_matrix = request.session['p_matrix']
+        x_y_data = {"x": selected_matrix[selected_numbers[0] - 1], "y": selected_matrix[selected_numbers[1] - 1]}
     elif param == 4:
         selected_matrix = request.session['calculated_average']
         donor_col[0], donor_col[1] = selected_matrix['donor'], selected_matrix['donor_waves']
         mm_col[0], mm_col[1] = selected_matrix['patient'], selected_matrix['patient_waves']
         non_mm_col[0], non_mm_col[1] = selected_matrix['non_secreting'], selected_matrix['non_secreting']
         x_y_data = {"donor": donor_col, "myeloma": mm_col, "non_myeloma": non_mm_col, "unknown": unknown}
+        print(x_y_data)
     elif param == 5:
         selected_matrix = request.session['calculated_samples']
         mm_name = []
         non_name = []
         for name in range(len(filenames)):
-            if filenames[name][0] == ("D" or "H"):
+            print(filenames[name][0])
+            if (filenames[name][0] == 'D') or (filenames[name][0] == 'H'):
                 pass
-            elif filenames[name][0] == ("M" or "P"):
+            elif (filenames[name][0] == 'M') or (filenames[name][0] == 'P'):
                 mm_name.append(filenames[name])
             elif (filenames[name][0] == "N") or (filenames[name][0:3] == "Non"):
                 non_name.append(filenames[name])
         for i in range(len(mm_name)):
             for num in selected_numbers:
+                print(mm_name[i][1:], num)
                 try:
-                    if mm_name[i][1:] == num:
+                    if (mm_name[i][1:] == num):
                         mm_col[0].append(selected_matrix['patient'][i])
                         mm_col[1].append(selected_matrix['patient_waves'][i])
-                    elif non_name[i][1:] == num:
+                    elif (non_name[i][1:] == num):
                         non_mm_col[0].append(selected_matrix['non_secreting'][i])
                         non_mm_col[1].append(selected_matrix['non_secreting_waves'][i])
                     else:
